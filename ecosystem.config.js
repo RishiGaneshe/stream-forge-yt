67a05826f36640dd7901a6920dc438220a34b76c
Abhishek Ganeshe <abhishekganeshe33@gmail.com>
2026-01-02 01:03:27 +0530

    [ Added .yml file ]

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [{
+      name: "stream-forge",
+      script: "app.js",
+      cwd: __dirname,
+      instances: 1,
+      exec_mode: "fork",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M",
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }]
+  }
+  
