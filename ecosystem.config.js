59f522b1a133ae15377152506f0c025b2cd9801c
Abhishek Ganeshe <abhishekganeshe33@gmail.com>
2026-01-02 01:45:56 +0530

    [ Needed Change ]

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,15 @@ module.exports = {
     apps: [{
       name: "stream-forge",
       script: "server.js",
-      cwd: __dirname,
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "300M",
       env_production: {
-        NODE_ENV: "production"
-      }
+        NODE_ENV: "production",
+        PORT: 10000
+      }      
     }]
   }
   
